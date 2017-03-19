70ea4e87a7a372943b72d95897a6a34a7017f0c7
premagopu <premagopu7@gmail.com>
2017-03-19 19:26:04 -0400

    Updated sprint 2 changes

--- parser.java
+++ parser.java
@@ -443,65 +443,6 @@ public class parser {
 		System.out.println("FAMILY: US01: Validated all dates before current date");
 	}
 	
-	// Validate marriage after 14
-	public HashMap<String, Object> mar = new HashMap<String, Object>();
-			public void validateMarriage(){
-				
-				for (Individual indl : individuals_list) {
-
-					mar.put(indl.getId(), indl);
-				}
-
-				for (Family fam : families_list) {
-					if(fam.getWeddingDate()!=null){
-					String fid = fam.getId();
-					String husband_id = fam.getHusbandId();
-					String wife_id = fam.getWifeId();
-					Individual i = (Individual) mar.get(husband_id);
-					Individual w = (Individual) mar.get(wife_id);
-					if (i.getAge() >= 14 && w.getAge() >= 14) {
-						//System.out.println();
-					}
-					else if(i.getAge()<14){
-						System.out.println("ERROR: FAMILY: US10: Marriage before 14 - Family: "+fid+" Husband: "+husband_id+" age is less than 14");
-					}
-					else if(w.getAge()<14){
-						System.out.println("ERROR: FAMILY: US10: Marriage before 14 - Family: "+fid+" Wife: "+wife_id+" age is less than 14");
-					}
-					else{
-						System.out.println("ERROR: FAMILY: US10: Marriage before 14 - Family: "+fid+" Husband ID: "+husband_id+" Wife ID: "+wife_id);
-					}
-					}
-					
-					}
-				}
-	
-	// Unique name and birth date validation	
-			public void validateUniqueNameBirthDate(){
-				List<String> allList = new ArrayList<String>();
-				for(Individual indd : individuals_list){
-					//String id = indd.getId();
-					String iname = indd.getName().replace("/", "").trim();
-					LocalDate dob = convertToLocalDate(indd.getBirthDate());
-					String namedob = iname+" "+dob.toString().trim();
-					allList.add(namedob);
-				}
-					Set<String> uniqueSet = new HashSet<String>();
-					List<String> dupesList = new ArrayList<String>();
-					for (String s : allList) {
-						if (uniqueSet.contains(s)) {
-							dupesList.add(s);
-						} else {
-							uniqueSet.add(s);
-						}
-					}
-					if (dupesList.isEmpty()) {
-					
-					} else {
-					System.out.println("ERROR: INDIVIDUAL: US23: Duplicate name and birthdate found " + dupesList);
-					}
-				System.out.println("INDIVIDUAL: US23: Validated all unique name and birthdate");
-			}
 	
 	// Gender Role Validation
 		public void genderRoleValidate() {
